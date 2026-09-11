4d7ef06c0077354c52b72a98f0d0e47dbb39fca8
Pogoy <pogoyarjay000@gmail.com>
2026-09-11 11:03:48 +1000

    add test driven folder - test/java

---
+++ src/test/java/com/wateradvisory/database/Arjay_Test/WaterConsumptionServiceTest.java
@@ -0,0 +1,4 @@
+package com.wateradvisory.database.Arjay_Test;
+
+public class WaterConsumptionServiceTest {
+}
--- src/test/java/com/wateradvisory/database/WaterConsumptionServiceTest.java
+++
@@ -1,4 +0,0 @@
-package com.wateradvisory.database;
-
-public class WaterConsumptionServiceTest {
-}
