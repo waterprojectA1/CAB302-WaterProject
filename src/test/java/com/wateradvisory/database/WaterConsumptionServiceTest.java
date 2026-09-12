c07e126aa98c4a1ffaba94c929ae88c28fa02267
Pogoy <pogoyarjay000@gmail.com>
2026-09-12 14:07:12 +1000

    database/WaterConsumptionServiceTest.java - Test 2 (Fail)
    test: null activity should return zero
    
    Output:
    NullPointerException
    java.lang.NullPointerException: Cannot invoke "String.hashCode()" because "<local5>" is null

--- src/test/java/com/wateradvisory/database/WaterConsumptionServiceTest.java
+++ src/test/java/com/wateradvisory/database/WaterConsumptionServiceTest.java
@@ -1,12 +1,11 @@
 package com.wateradvisory.database;
 
-import com.wateradvisory.database.WaterConsumptionService;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WaterConsumptionServiceTest {
 
+    // Test 1 - Negative Duration should return 0
     @Test
     void negativeDurationShouldReturnZero() {
 
@@ -14,6 +13,24 @@ public class WaterConsumptionServiceTest {
                 WaterConsumptionService.calculateActivity(
                         "Shower",
                         -5,
+                        2
+                );
+
+        assertEquals(
+                0.0,
+                result,
+                0.001
+        );
+    }
+
+    // Test 2 - Null Activity should return 0
+    @Test
+    void nullActivityShouldReturnZero() {
+
+        double result =
+                WaterConsumptionService.calculateActivity(
+                        null,
+                        5,
                         2
                 );
 
